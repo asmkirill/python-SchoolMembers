72ee965987413f75708d51689f6ccdd8ddc6c799
Kiryl <101750136+asmkirill@users.noreply.github.com>
2023-03-02 19:41:36 +0400

    Add files via upload
    Common task for beginners Python developers

---
+++ task.py
@@ -0,0 +1,34 @@
+"""
+    By defining the SchoolMember, Teacher, and Student classes in this way,
+    we can create instances of each class and access their attributes and methods.
+    We can also use inheritance to reuse the code of the SchoolMember class in the Teacher and Student classes,
+    and override methods as needed in the child classes.
+"""
+class SchoolMember:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    def show(self):
+        return str()
+
+class Teacher(SchoolMember):
+    def __init__(self, name, age, salary):
+        SchoolMember.__init__(self, name, age)
+        self.salary = salary
+    def show(self):
+        SchoolMember.show(self)
+        return f"Name: {self.name}, Age: {self.age}, Salary: {self.salary}"
+
+class Student(SchoolMember):
+    def __init__(self, name, age, grade):
+        SchoolMember.__init__(self, name, age)
+        self.grade = grade
+
+    # Method that returns a string representation of a Teacher object
+    # Overrides the show method in the parent class to include the salary attribute
+
+    def show(self):
+        SchoolMember.show(self)
+        return f"Name: {self.name}, Age: {self.age}, Grades: {self.grade}"
+
+
