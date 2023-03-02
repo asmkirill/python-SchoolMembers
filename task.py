a451fa7764ba399fa2fb90053e66e269993d2dd9
Kiryl <101750136+asmkirill@users.noreply.github.com>
2023-03-02 19:42:34 +0400

    Update task.py

--- task.py
+++ task.py
@@ -1,9 +1,11 @@
+
 """
     By defining the SchoolMember, Teacher, and Student classes in this way,
     we can create instances of each class and access their attributes and methods.
     We can also use inheritance to reuse the code of the SchoolMember class in the Teacher and Student classes,
     and override methods as needed in the child classes.
 """
+
 class SchoolMember:
     def __init__(self, name, age):
         self.name = name
